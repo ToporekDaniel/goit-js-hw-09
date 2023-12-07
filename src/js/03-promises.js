e60bfd4116e06893d2a6b538a47c9b97055925c3
ToporekDaniel <danieltoporek0@gmail.com>
2023-12-07 22:53:34 +0100

    zad 3 początek

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,10 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="step"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +13,7 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(delay.value, step.value, amount.value);
+});
